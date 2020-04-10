27a09f29b54b0797d927a4bed68fec4b7a416ebb
Anosh D. Ullenius <anosh@anosh.se>
2020-04-10 08:41:42 +0200

    Minor. Clean up imports

--- src/main/java/se/anosh/gbs/service/Gbs.java
+++ src/main/java/se/anosh/gbs/service/Gbs.java
@@ -2,8 +2,6 @@ package se.anosh.gbs.service;
 
 import java.io.IOException;
 
-import se.anosh.gbs.domain.ReadOnlyGbsTag;
-import se.anosh.gbs.domain.SimpleGbsTag;
 import se.anosh.gbs.domain.Tag;
 
 public interface Gbs {
